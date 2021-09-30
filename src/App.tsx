5adde8f1c2d38e0b8b1f30b15ba15d701dd77b2f
Vlad Volokitin <34795095+volokitty@users.noreply.github.com>
2021-09-30 14:04:54 +0300

    add prettier and stylelint-prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      React
-    </div>
-  );
+    return <div className="App">React</div>;
 }
 
 export default App;
